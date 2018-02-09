e49bec3beae0fc1a41db0363fbf3d41b52af2efa
seraphinagoldfarb-tarrant <serif@uw.edu>
2018-02-08 17:31:17 -0800

    Finished and bug free versions of the model calculation scripts.

--- hw5/calc_emp_exp.py
+++ hw5/calc_emp_exp.py
@@ -26,33 +26,27 @@ def calc_emp_exp(train_records):
     return emp_exp_dict
 
 
-def write_expec(emp_exp_dict, num_records, outputfilename, type):
+def write_expec(exp_dict, num_records, outputfilename, type):
     '''
     writes expectations to a file in form: 'class_label feature expectation raw_count'
-    :param emp_exp_dict: the nested dict Counter of form {class_label: {feature: count}}
+    :param exp_dict: the nested dict Counter of form {class_label: {feature: count}}
     :param num_records: 'N' in maxent terminology - the total number of samples
     :param outputfilename: filename to write data to
-    :param type: The type of expectation being written - model or empirical. Empirical will have Count and we will have
-    to calculate expectation, and model will be the reverse.
+    :param type: the type to write: empirical or model. Decides the formatting of raw count which will be float or int
     :return: None - writes to file
     '''
     output_data = []
-    class_list = sorted(list(emp_exp_dict))  # a sorted list of all classes
-    feature_set = set()  # create a set of all features to use in calculations. This is if I want zeros to show up when
-    # a feature never appeared in a class. It will be slower though since I will iterate over the whole vocab rather
-    # than just features present in a given class. But it is truer to the MaxEnt formula
-    [feature_set.update(set(emp_exp_dict[c])) for c in emp_exp_dict]
-    all_features = sorted(list(feature_set))
+    class_list = sorted(list(exp_dict))  # a sorted list of all classes
     for class_label in class_list:
-        #feature_list = list(emp_exp_dict[class_label]).sort()  # a sorted list of features. If I don't want to iterate over the whole vocab
-        for feat in all_features:
-            val = emp_exp_dict[class_label][feat]
+        feature_list = sorted(list(exp_dict[class_label]))  # a sorted list of features. If I don't want to iterate over the whole vocab
+        for feat in feature_list:
+            val = exp_dict[class_label][feat]
             # if data is empirical, the val is a raw count and the expectation is val/num_records
             if type == 'empirical':
-                output_data.append('{} {} {:.5f} {}'.format(class_label, feat, val/num_records, val))
+                output_data.append('{} {} {:.5f} {}'.format(class_label, feat, val / num_records, val))
             # if data is model expectation, the val is the expectation and the raw count is expectation * num_records (printed as int)
             elif type == 'model':
-                output_data.append('{} {} {:.5f} {:.0f}'.format(class_label, feat, val, val*num_records))
+                output_data.append('{} {} {:.5f} {:.5f}'.format(class_label, feat, val / num_records, val))
             else:
                 print('Invalid type of expectation given: please give either "empirical" or "model"', file=sys.stderr)
 
--- hw5/calc_model_exp.py
+++ hw5/calc_model_exp.py
@@ -9,12 +9,13 @@ Command to run: calc_model_exp.py training_data output_file {model_file} where m
 
 import argparse
 from collections import defaultdict, Counter
-
+from math import exp
+import numpy as np
 from maxent_classify import svm_light_to_binary_features, read_maxent_model
 from calc_emp_exp import write_expec
 
 
-def calc_model_exp(train_records, model_weights=None):
+def calc_model_exp(train_records, model_weights=None, class_weights=None):
     '''
     Calculates model expectation given training records, and possibly a MaxEnt model. Note that it does not divide by
     number of records - that is done at the print step.
@@ -24,20 +25,39 @@ def calc_model_exp(train_records, model_weights=None):
     '''
     model_exp_dict = defaultdict(Counter)
     if model_weights:
-        all_classes = list(model_weights)
+        all_classes = sorted(list(model_weights))
     else:
-        all_classes = set([rec[0] for rec in train_records])
+        all_classes = sorted(list(set([rec[0] for rec in train_records])))
     num_classes = len(all_classes)
     for record in train_records:
         label, features = record
+        class_results = [] # for storing p(class|doc) aka p(y|x)
+        # if model was given, calculate p(y|x) for all classes, just like in decoding. Next time, make this a function
+        if model_weights:
+            Z_total = 0
+            for class_label in all_classes:
+                sum = class_weights[class_label] # will never have model weights without class weights
+                for feature in features:
+                    sum += model_weights[class_label][feature]
+                sum = exp(sum)
+                Z_total += sum
+                class_results.append(sum)
+            class_results = (np.array(class_results)) / Z_total
+        else: # if no model, make class results array uniform
+            class_results = [1/num_classes for i in range(num_classes)]
+
         # since this is the model, we pretend we do not know the class label and iterate through all of them
-        for class_label in all_classes:
+        for i in range(num_classes): # want to be able to use coindexing for current class and class_results array
             for feat in features:
+                model_exp_dict[all_classes[i]][feat] += class_results[i]
+                ''''
                 # if a model was given use that, else the weight will be 1/num_classes (ie maximum entropy)
                 if model_weights:
-                    model_exp_dict[class_label][feat] += model_weights[class_label][feat]
+                    prob_y_g_x = exp(class_weights[class_label]+)
+                    model_exp_dict[class_label][feat] += p(y|x) #model_weights[class_label][feat]
                 else:
                     model_exp_dict[class_label][feat] += 1/num_classes
+                '''
 
     return model_exp_dict
 
@@ -55,10 +75,10 @@ if __name__ == "__main__":
     if args.model_filename:
         c_weights, f_weights = read_maxent_model(args.model_filename)
     else:
-        f_weights = None
+        c_weights, f_weights = None, None
     # if so, read model if not, initialise a class_weights dict where each
     train_records = svm_light_to_binary_features(args.train_filename)
-    expectations = calc_model_exp(train_records, f_weights)
+    expectations = calc_model_exp(train_records, f_weights, c_weights)
     write_expec(expectations, len(train_records), args.output_filename, 'model')
 
     
